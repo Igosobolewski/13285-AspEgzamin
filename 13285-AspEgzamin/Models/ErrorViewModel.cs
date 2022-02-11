b00aac6ed3a9cae62b7a3b605daecff6216e53f2
Igosobolewski <igosobolewski@gmail.com>
2022-02-11 09:42:02 +0100

    Dodaj pliki projektów.

---
+++ 13285-AspEgzamin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _13285_AspEgzamin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
